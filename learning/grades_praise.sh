952d66b1e8b4b36c3ca280329ade61d29c08fa65
ckafrouni <ckafrouni@proton.me>
2024-01-03 15:58:16 +0100

    Intro to functions

--- learning/grades_praise.sh
+++ learning/grades_praise.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 
-read -p "What is your grade [0..20] ? " GRADE
+evaluate_grade() {
+	grade=$1
+	s=$2;
+	e=$3;
+	msg=$4;
 
-# check if out of bounds
-if [ -z "$GRADE" ] || [ $GRADE -lt 0 -o $GRADE -gt 20 ]; then
-	echo "ERROR: invalid grade: '$GRADE'" >&2;
-	exit 1;
-fi
+	if [ "$grade" -ge "$s" -a "$grade" -le "$e" ]; then
+		echo $msg;
+		exit 0;
+	fi
+}
 
-if [ "$GRADE" -lt 10  ]; then
-	echo "Bad";
-	exit 0;
-elif [ "$GRADE" -lt 12  ]; then
-	echo "Enough";
-	exit 0;
-elif [ "$GRADE" -lt 14  ]; then
-	echo "Good";
-	exit 0;
-elif [ "$GRADE" -lt 16  ]; then
-	echo "Great";
-	exit 0;
-else
-	echo "Amazing";
-	exit 0;
-fi
+read -p "What is your grade [0..20] ? " grade
 
+evaluate_grade $grade 0 9 "Bad"
+evaluate_grade $grade 10 12 "Enough"
+evaluate_grade $grade 12 14 "Good"
+evaluate_grade $grade 14 16 "Great"
+evaluate_grade $grade 16 20 "Amazing"
+
+echo "ERROR: bad grade '$grade'" >&2
+exit 1;
